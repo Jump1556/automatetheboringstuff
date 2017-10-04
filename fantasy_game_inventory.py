eabd0db04d65d87116871533677907fad0d583a6
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-04 11:54:32 +0200

    Fantasy_game_inventory update. Function add_to_inventory added.

--- fantasy_game_inventory.py
+++ fantasy_game_inventory.py
@@ -1,3 +1,5 @@
+#Fantasy Game Inventory
+
 inventory = {'arrow': 12, 'gold coin': 42, 'rope': 1, 'torch': 6, 'dagger': 1}
 
 def display_inventory(my_dict):
@@ -5,5 +7,18 @@ def display_inventory(my_dict):
     for k, v in my_dict.items():
         print(v, k)
     print("Total number of items: " + str(sum(my_dict.values())))
+#print(display_inventory(inventory))
 
-print(display_inventory(inventory))
+
+#List to Dictionary Function for Fantasy Game Inventory
+
+def add_to_inventory(my_dict, added_items):
+  for i in added_items:
+    my_dict.setdefault(i, 0)
+    my_dict[i] = my_dict[i] + 1
+  return my_dict
+
+inv = {'gold coin':42, 'rope':1}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+add_to_inventory(inv, dragon_loot)
+print(display_inventory(inv))
