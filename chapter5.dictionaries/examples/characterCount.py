bee71f9e2f69bec92e6af29a652ea3fe4835be0a
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-11 22:56:24 +0200

    added folders with examples and tasks with using dictionaries

---
+++ chapter5.dictionaries/examples/characterCount.py
@@ -0,0 +1,11 @@
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
+#print(count) for printing in 1 row
