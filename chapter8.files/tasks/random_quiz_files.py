377d39db2c56cee73537ab71d37242a004d251a2
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-16 11:23:33 +0200

    Write Content to the Quiz and Answer Key Files

--- chapter8.files/tasks/random_quiz_files.py
+++ chapter8.files/tasks/random_quiz_files.py
@@ -25,22 +25,24 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
 for quiz_num in range(35):
     quiz_file = open('capitalsquiz%s.txt' % (quiz_num +1), 'w')
     answer_key = open('capitalsquiz_anwsers%s.txt' % (quiz_num +1), 'w')
+    quiz_file.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quiz_file.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quiz_num +1))
+    quiz_file.write('\n\n')
+    states = list(capitals.keys())
+    random.shuffle(states)
 
-quiz_file.write('Name:\n\nDate:\n\nPeriod:\n\n')
-quiz_file.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quiz_num +1))
-quiz_file.write('\n\n')
+for question_num in range(50):
+    correct_answ = capitals[states[question_num]]
+    wrong_answ = list(capitals.values())
+    del wrong_answ[wrong_answ.index(correct_answ)]
+    wrong_answ = random.sample(wrong_answ, 3)
+    anws_options = wrong_answ + [correct_answ]
+    random.shuffle(anws_options)
+    quiz_file.write('%s. What is the capital of %s?\n' % (question_num + 1, states[question_num]))
+    for i in range(4):
+        quiz_file.write(' %s. %s\n' % ('ABCD'[i], anws_options[i]))
+    quiz_file.write('\n')
+    answer_key.write('%s. %s\n' % (question_num + 1, 'ABCD'[anws_options.index(correct_answ)]))
 
-states = list(capitals.keys())
-random.shuffle(states)
-
-for quiz_num in range(50):
-   correct_answ = capitals[states[question_num]]
-   wrong_answ = list(capitals.values())
-   del wrong_answ[wrong_answ.index(correct_answ)]
-   wrong_answ = random.sample(wrong_answ, 3)
-   anws_options = wrong_answ + [correct_answ]
-   random.shuffle(anws_options)
-
- # TODO: Write the question and answer options to the quiz file.
-
-# TODO: Write the answer key to a file.
+quiz_file.close()
+answer_key.close()
