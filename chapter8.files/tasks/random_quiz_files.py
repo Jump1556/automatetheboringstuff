7803d940866c072abc078ebeb50a1cd5c74f6db8
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-16 10:04:24 +0200

    Create the Quiz File and Shuffle the Question Order

--- chapter8.files/tasks/random_quiz_files.py
+++ chapter8.files/tasks/random_quiz_files.py
@@ -13,20 +13,24 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
    'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston', 'Michigan':
    'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson', 'Missouri':
    'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln', 'Nevada':
-   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New
-   Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+   'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton', 'New Mexico':
+   'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
    'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
    'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
    'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
    'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West
-   Virginia': 'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+   'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 'West Virginia':
+   'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
 for quiz_num in range(35):
-        # TODO: Create the quiz and answer key files.
+    quiz_file = open('capitalsquiz%s.txt' % (quiz_num +1), 'w')
+    answer_key = open('capitalsquiz_anwsers%s.txt' % (quiz_num +1), 'w')
 
-       # TODO: Write out the header for the quiz.
+quiz_file.write('Name:\n\nDate:\n\nPeriod:\n\n')
+quiz_file.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quiz_num +1))
+quiz_file.write('\n\n')
 
-       # TODO: Shuffle the order of the states.
+states = list(capitals.keys())
+random.shuffle(states)
 
        # TODO: Loop through all 50 states, making a question for each.
