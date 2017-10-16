958d5ea8883bb35afd9260d74aa925092e5a45c9
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-16 09:20:02 +0200

    Program that is a Version of strip() method update

--- chapter7.regex/tasks/version_of_strip.py
+++ chapter7.regex/tasks/version_of_strip.py
@@ -8,10 +8,4 @@ def stripRegex(s, toStrip='' ):
     pattern= re.compile(r'^{0}|{0}$'.format(toStrip))
     return pattern.sub('', s)
 
-print(stripRegex("hh hgts hh", "hh "))
-
-string = "tas"
-string_new = "Ana{0}ia {1} - Valdenmaiier ".format(string, "Kostashenko")
-string_new = "Anas%sia" % string
-string_new = "Anas" + string + "ia"
-print(string_new)
+print(stripRegex("hh hgts hh", "hh"))
