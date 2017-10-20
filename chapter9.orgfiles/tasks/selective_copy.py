e43cab077c510bd41564938940b70a8d178e0938
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-20 19:56:48 +0200

    task done

--- chapter9.orgfiles/tasks/selective_copy.py
+++ chapter9.orgfiles/tasks/selective_copy.py
@@ -4,4 +4,21 @@
 # Copy these files from whatever location they are in to a new folder.
 
 #TODO walk throw folder tree and find pdf anf jpg files
+#TODO create new folder
 #TODO copy files put them in new folder
+
+import os, shutil
+
+def selective_copy(path):
+    for dirname, dirnames, filenames in os.walk(path):
+        print('The current folder is ' + dirname)
+        new_path = (path + 'new_folder/')
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+        for filename in filenames:
+            path_name = dirname + '/' + filename
+            if filename.endswith('.pdf') or filename.endswith('.jpg'):
+                shutil.copy(path_name, new_path)
+
+selective_copy()
+
