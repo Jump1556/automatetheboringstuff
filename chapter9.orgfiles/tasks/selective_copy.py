db4884d17d87fdcd4fb0524b10300f7a29ab88f6
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-20 15:30:22 +0200

    task writen

---
+++ chapter9.orgfiles/tasks/selective_copy.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+# selective_copy.py - a program that walks through a folder tree
+# and searches for files with a certain file extension (such as .pdf or .jpg).
+# Copy these files from whatever location they are in to a new folder.
+
+#TODO walk throw folder tree and find pdf anf jpg files
+#TODO copy files put them in new folder
