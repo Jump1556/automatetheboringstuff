4e87f4a042df1e22725b84054ddfb0117b174f49
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-22 18:46:16 +0200

    task completed

--- chapter9.orgfiles/tasks/delete_unneeded_files.py
+++ chapter9.orgfiles/tasks/delete_unneeded_files.py
@@ -2,6 +2,12 @@
 #delete_unneeded_files.py - program walk through a folder tree,
 #search for files more than 100MB and print files names and their absolute path
 
-# TODO! 1. walk through folder tree
-# TODO! 2. find large files
-# TODO! 3. print files and paths
+
+import os
+
+path = '/Users/anastasia/osmakedir folder'
+
+for foldername, subfolders, filenames in os.walk(path):
+         for filename in filenames:
+             if int(os.path.getsize(foldername + '/' + filename)) > 104857600:
+                 print(os.path.abspath(foldername + '/' + filename))
