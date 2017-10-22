9cd3bfab620958b1c63964b25386226cb26818ec
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-22 18:24:50 +0200

    delete todo comments

--- chapter9.orgfiles/tasks/selective_copy.py
+++ chapter9.orgfiles/tasks/selective_copy.py
@@ -3,9 +3,6 @@
 # and searches for files with a certain file extension (such as .pdf or .jpg).
 # Copy these files from whatever location they are in to a new folder.
 
-#TODO walk throw folder tree and find pdf anf jpg files
-#TODO create new folder
-#TODO copy files put them in new folder
 
 import os, shutil
 
