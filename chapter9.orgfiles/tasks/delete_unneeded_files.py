a98d30dac38205b6753c17a6d0d7360c24ba6723
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-22 18:05:27 +0200

    Structured task

---
+++ chapter9.orgfiles/tasks/delete_unneeded_files.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+#delete_unneeded_files.py - program walk through a folder tree,
+#search for files more than 100MB and print files names and their absolute path
+
+# TODO! 1. walk through folder tree
+# TODO! 2. find large files
+# TODO! 3. print files and paths
