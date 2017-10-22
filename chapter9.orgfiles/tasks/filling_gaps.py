3ce606186f6f175746b3681fb2570a8e1a241728
Jump1556 <anastasiia.kostashenko@gmail.com>
2017-10-22 20:54:28 +0200

    task completed in a best way

---
+++ chapter9.orgfiles/tasks/filling_gaps.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+#filling_gaps.py - finds all files with a given prefix and locates any gaps in the numbering,
+# renames all the later files to close this gap.
+
+
+import os, shutil
+
+folder = input('Provide folder: ')
+prefix = input('Provide prefix: ')
+filenames = []
+for filename in os.listdir(folder):
+    if filename.startswith(prefix):
+        filenames.append(filename)
+print('List of files in the folder: ' + str(filenames))
+
+index = 0
+for filename in filenames:
+    index += 1
+    new_filename = prefix + str('%02d' % index) +'.txt'
+    shutil.move((folder + filename), (folder + new_filename))
+print('Files renamed without gaps in numbers.')
+
+
+
